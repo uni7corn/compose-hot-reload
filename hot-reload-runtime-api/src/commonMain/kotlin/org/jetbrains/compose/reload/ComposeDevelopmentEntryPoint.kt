8f41f28f59166bacd2cf1484bd7f8087fcae14f4
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-10-22 10:48:17 +0200

    1.0.0-dev.21

--- hot-reload-runtime-api/src/commonMain/kotlin/org/jetbrains/compose/reload/ComposeDevelopmentEntryPoint.kt
+++ hot-reload-runtime-api/src/commonMain/kotlin/org/jetbrains/compose/reload/ComposeDevelopmentEntryPoint.kt
@@ -3,4 +3,12 @@ package org.jetbrains.compose.reload
 import androidx.compose.runtime.Composable
 
 @Composable
+@Deprecated("Use 'ComposeDevelopmentEntryPoint' instead", ReplaceWith("ComposeDevelopmentEntryPoint(child)"))
+public fun Hot(child: @Composable () -> Unit): Unit = ComposeDevelopmentEntryPoint(child)
+
+@Composable
+@Deprecated("Use 'ComposeDevelopmentEntryPoint' instead", ReplaceWith("ComposeDevelopmentEntryPoint(child)"))
+public fun HotReload(child: @Composable () -> Unit): Unit = ComposeDevelopmentEntryPoint(child)
+
+@Composable
 public expect fun ComposeDevelopmentEntryPoint(child: @Composable () -> Unit)
