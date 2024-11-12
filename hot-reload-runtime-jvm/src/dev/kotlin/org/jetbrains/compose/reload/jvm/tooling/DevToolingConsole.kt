f6bcd08150baf30c4b724ed0791d9c42ff317b7a
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-11-12 07:23:43 +0100

    Fix DevToolingConsole crashing in measure

--- hot-reload-runtime-jvm/src/dev/kotlin/org/jetbrains/compose/reload/jvm/tooling/DevToolingConsole.kt
+++ hot-reload-runtime-jvm/src/dev/kotlin/org/jetbrains/compose/reload/jvm/tooling/DevToolingConsole.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -21,25 +23,28 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterIsInstance
 import org.jetbrains.compose.reload.agent.ComposeHotReloadAgent
+import org.jetbrains.compose.reload.jvm.createLogger
 import org.jetbrains.compose.reload.orchestration.OrchestrationMessage.LogMessage
 import org.jetbrains.compose.reload.orchestration.asFlow
 
 @Composable
 fun DevToolingConsole(tag: String, modifier: Modifier) {
+    val listState = rememberLazyListState()
     var outputLines by remember { mutableStateOf(listOf<String>()) }
 
     LaunchedEffect(Unit) {
         ComposeHotReloadAgent.orchestration.asFlow().filterIsInstance<LogMessage>()
             .filter { message -> message.tag == tag }
-            .collect { value -> outputLines = (outputLines + value.message).takeLast(1024) }
+            .collect { value ->
+                outputLines = (outputLines + value.message).takeLast(2048)
+                listState.scrollToItem(outputLines.lastIndex)
+            }
     }
 
 
     Row(modifier) {
         Card(Modifier.padding(16.dp).fillMaxWidth()) {
             Box(Modifier.padding(16.dp)) {
-                val listState = LazyListState(outputLines.lastIndex)
-
                 LazyColumn(state = listState) {
                     items(outputLines) { text ->
                         Row {
