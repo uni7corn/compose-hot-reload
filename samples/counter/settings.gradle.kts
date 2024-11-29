eec3f872d581470d4e0fcd8bb6ba4353cce5ac28
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-11-29 14:16:25 +0100

    v1.0.0-dev.29.2

--- samples/counter/settings.gradle.kts
+++ samples/counter/settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose-hot-reload") version "1.0.0-dev.29.1"
+        id("org.jetbrains.compose-hot-reload") version "1.0.0-dev.29.2"
     }
 
     repositories {
