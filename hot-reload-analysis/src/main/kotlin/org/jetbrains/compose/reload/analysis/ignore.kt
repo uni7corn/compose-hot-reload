b65b623f792a15bb17cf0e2366478ce0da329044
Sebastiano Poggi <sebp@google.com>
2024-12-09 18:49:03 +0100

    Add more IJP-related packages to ignores

--- hot-reload-analysis/src/main/kotlin/org/jetbrains/compose/reload/analysis/ignore.kt
+++ hot-reload-analysis/src/main/kotlin/org/jetbrains/compose/reload/analysis/ignore.kt
@@ -7,10 +7,14 @@ internal fun isIgnoredClassId(classId: String): Boolean = with(classId) {
             startsWith("javax/") ||
             startsWith("java/") ||
             startsWith("jdk/") ||
+            startsWith("com/sun/") ||
+            startsWith("io/netty/") ||
             startsWith("sun/") ||
             startsWith("kotlin/") ||
             startsWith("kotlinx/") ||
             startsWith("androidx/") ||
             startsWith("org/jetbrains/skia/") ||
-            startsWith("org/jetbrains/skiko/")
+            startsWith("org/jetbrains/skiko/") ||
+            startsWith("com/intellij") ||
+            startsWith("com/jetbrains")
 }
