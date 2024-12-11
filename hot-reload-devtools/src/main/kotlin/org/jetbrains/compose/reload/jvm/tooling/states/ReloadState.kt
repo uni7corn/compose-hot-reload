75316a228f4d1e16be415d6b0782579123916056
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-12-11 14:34:45 +0100

    Fix reload state getting green after one second (when redefinition failed)

--- hot-reload-devtools/src/main/kotlin/org/jetbrains/compose/reload/jvm/tooling/states/ReloadState.kt
+++ hot-reload-devtools/src/main/kotlin/org/jetbrains/compose/reload/jvm/tooling/states/ReloadState.kt
@@ -3,16 +3,12 @@ package org.jetbrains.compose.reload.jvm.tooling.states
 import io.sellmair.evas.State
 import io.sellmair.evas.launchState
 import io.sellmair.evas.update
-import io.sellmair.evas.value
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import org.jetbrains.compose.reload.jvm.tooling.orchestration
 import org.jetbrains.compose.reload.orchestration.OrchestrationMessage
 import org.jetbrains.compose.reload.orchestration.asFlow
-import kotlin.time.Duration.Companion.seconds
 
 sealed class ReloadState : State {
 
@@ -36,23 +32,14 @@ sealed class ReloadState : State {
     }
 }
 
+
 fun CoroutineScope.launchReloadState() = launchState(ReloadState) {
+    var currentReloadRequest: OrchestrationMessage.ReloadClassesRequest? = null
+
     orchestration.asFlow().collect { message ->
-        if (message is OrchestrationMessage.ReloadClassesResult) {
-            emit(
-                if (message.isSuccess) ReloadState.Ok()
-                else ReloadState.Failed("Failed reloading classes (${message.errorMessage})")
-            )
-        }
-
-        if (message is OrchestrationMessage.ReloadClassesRequest) {
-            ReloadState.update { state ->
-                state as? ReloadState.Reloading ?: ReloadState.Reloading(time = Clock.System.now())
-            }
-        }
-
         if (message is OrchestrationMessage.LogMessage && message.tag == OrchestrationMessage.LogMessage.TAG_COMPILER) {
             if (message.message.contains("executing build...")) {
+                currentReloadRequest = null
                 ReloadState.Reloading(time = Clock.System.now()).emit()
             }
 
@@ -61,24 +48,31 @@ fun CoroutineScope.launchReloadState() = launchState(ReloadState) {
             }
 
             /*
-            The build was successful, but this does not necessarily mean that a reload request is fired.
+            The build was successful, but this doesn't necessarily mean that a reload request is fired.
             Example: Lets say some code was added, which causes a 'BUILD FAILED'.
             Afterward, the failing code was removed again. A 'BUILD SUCCESSFUL' will be observed w/o firing
             a reload request, because the runtime classpath is not changed to the last working state.
 
-            Because of this, a new coroutine is launched after 'BUILD SUCCESSFUL' to only update
-            the reload state to 'Ok' after a debounced delay of 1 second.
+            Because of this, we will set the state to 'OK' if the build was successful, but no
+            reload request was observed.
              */
             if (message.message.contains("BUILD SUCCESSFUL")) {
-                val capturedState = ReloadState.value()
-                launch {
-                    delay(1.seconds)
-                    ReloadState.update { currentState ->
-                        if (currentState === capturedState) ReloadState.Ok(time = Clock.System.now())
-                        else currentState
-                    }
+                if (currentReloadRequest == null) {
+                    ReloadState.update { state -> state as? ReloadState.Ok ?: ReloadState.Ok() }
                 }
             }
+        }
+
+        if (message is OrchestrationMessage.ReloadClassesRequest) {
+            currentReloadRequest = message
+            ReloadState.update { state ->
+                state as? ReloadState.Reloading ?: ReloadState.Reloading(time = Clock.System.now())
+            }
+        }
+
+        if (message is OrchestrationMessage.ReloadClassesResult) {
+            if(message.isSuccess) ReloadState.Ok(time = Clock.System.now()).emit()
+            else ReloadState.Failed("Failed reloading classes (${message.errorMessage})").emit()
         }
     }
 }
