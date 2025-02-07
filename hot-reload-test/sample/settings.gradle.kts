aa44ee60f48933f1db677b98e57d0cde3be99b61
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-02-07 08:35:43 +0100

    v1.0.0-dev-37

--- hot-reload-test/sample/settings.gradle.kts
+++ hot-reload-test/sample/settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose-hot-reload") version "1.0.0-dev-36
-        id("org.jetbrains.compose-hot-reload-test") version "1.0.0-dev-36
+        id("org.jetbrains.compose-hot-reload") version "1.0.0-dev-37
+        id("org.jetbrains.compose-hot-reload-test") version "1.0.0-dev-37
     }
 
     repositories {
