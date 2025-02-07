502d3360b45e7adb7543e11905f02ea77e31f343
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-02-07 15:43:01 +0100

    Suppress unused on root 'build.gradle.kts'

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
-@file:Suppress("UnstableApiUsage")
+@file:Suppress("UnstableApiUsage", "unused")
 
 tasks.maybeCreate<Delete>("clean").apply {
     delete(layout.buildDirectory)
