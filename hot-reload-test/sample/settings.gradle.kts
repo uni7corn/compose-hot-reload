e6e6553b59bbcd1aa8d8374dbcb41aca20419b9c
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-02-07 08:20:14 +0100

    v1.0.0-dev-35

--- hot-reload-test/sample/settings.gradle.kts
+++ hot-reload-test/sample/settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose-hot-reload") version "1.0.0-dev.34.10"
-        id("org.jetbrains.compose-hot-reload-test") version "1.0.0-dev.34.10"
+        id("org.jetbrains.compose-hot-reload") version "1.0.0-dev-35
+        id("org.jetbrains.compose-hot-reload-test") version "1.0.0-dev-35
     }
 
     repositories {
--- samples/bytecode-analyzer/settings.gradle.kts
+++ samples/bytecode-analyzer/settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose-hot-reload") version "1.0.0-dev.34.10"
+        id("org.jetbrains.compose-hot-reload") version "1.0.0-dev-35
     }
 
     repositories {
