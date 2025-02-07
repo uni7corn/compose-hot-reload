d19ba6fd74a7351c2b8e035442800e3440dc1066
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-02-07 09:03:32 +0100

    Fix '1.0.0-dev-39' version declarations

--- hot-reload-test/sample/build.gradle.kts
+++ hot-reload-test/sample/build.gradle.kts
@@ -13,7 +13,7 @@ kotlin {
     jvm()
 
     sourceSets.jvmMain.dependencies {
-        implementation("org.jetbrains.compose:hot-reload-runtime-api:1.0.0-dev-39
+        implementation("org.jetbrains.compose:hot-reload-runtime-api:1.0.0-dev-39"
         implementation(compose.runtime)
     }
 
