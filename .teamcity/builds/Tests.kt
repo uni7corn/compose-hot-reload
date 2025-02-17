88326ce4345a06053fcf6e6aba3cb47fcdac521a
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-02-17 18:06:03 +0100

    Add --continue to tests

--- .teamcity/builds/Tests.kt
+++ .teamcity/builds/Tests.kt
@@ -83,7 +83,7 @@ class Test(
     steps {
         gradle {
             name = "Test"
-            tasks = "check -x apiCheck"
+            tasks = "check --continue -x apiCheck"
 
             /* Any host other than linux is considered to only run 'host integration tests' */
             if (requiredHost != Host.Linux) {
