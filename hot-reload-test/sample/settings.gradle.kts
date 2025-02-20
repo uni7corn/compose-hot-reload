0941d11d22aaecc2a2d90659149c52a595805f46
JetBrains Compose Team <compose-team@jetbrains.com>
2025-02-20 13:33:44 +0000

    v1.0.0-dev-60

--- hot-reload-test/sample/settings.gradle.kts
+++ hot-reload-test/sample/settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose.hot-reload") version "1.0.0-dev-59"
-        id("org.jetbrains.compose.hot-reload.test") version "1.0.0-dev-59"
+        id("org.jetbrains.compose.hot-reload") version "1.0.0-dev-60"
+        id("org.jetbrains.compose.hot-reload.test") version "1.0.0-dev-60"
     }
 
     repositories {
