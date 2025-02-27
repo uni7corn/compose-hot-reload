410874a3196748145ee81da5a4bd2e0cd1166fee
JetBrains Compose Team <compose-team@jetbrains.com>
2025-02-27 17:57:18 +0000

    v1.0.0-dev-65

--- samples/bytecode-analyzer/build.gradle.kts
+++ samples/bytecode-analyzer/build.gradle.kts
@@ -16,8 +16,8 @@ kotlin {
     }
 
     sourceSets.commonMain.dependencies {
-        implementation("org.jetbrains.compose.hot-reload:core:1.0.0-dev-64")
-        implementation("org.jetbrains.compose.hot-reload:analysis:1.0.0-dev-64")
+        implementation("org.jetbrains.compose.hot-reload:core:1.0.0-dev-65")
+        implementation("org.jetbrains.compose.hot-reload:analysis:1.0.0-dev-65")
 
         implementation("io.sellmair:evas:1.2.0")
         implementation("io.sellmair:evas-compose:1.2.0")
