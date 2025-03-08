b588bc85681868b4065209134d60aa7d01a1cffd
natanfudge <natan.lifsiz@gmail.com>
2025-03-08 18:43:46 +0200

    Replace Window with DialogWindow

--- hot-reload-devtools/src/main/kotlin/org/jetbrains/compose/reload/jvm/tooling/sidecar/DtSidecarWindow.kt
+++ hot-reload-devtools/src/main/kotlin/org/jetbrains/compose/reload/jvm/tooling/sidecar/DtSidecarWindow.kt
@@ -33,7 +33,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.DialogState
+import androidx.compose.ui.window.DialogWindow
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import kotlinx.coroutines.delay
@@ -104,12 +105,12 @@ fun DtSidecarWindow(
         yAnimatable.snapTo(targetY.value)
     }
 
-    val sidecarWindowState = WindowState(
+    val sidecarWindowState = DialogState(
         width = sideCarWidth, height = height,
         position = WindowPosition(x = x.dp, y = y.dp)
     )
 
-    Window(
+    DialogWindow(
         onCloseRequest = {
             orchestration.sendMessage(OrchestrationMessage.ShutdownRequest()).get()
             exitProcess(0)
@@ -121,10 +122,6 @@ fun DtSidecarWindow(
         focusable = true,
         alwaysOnTop = isAlwaysOnTop
     ) {
-
-        LaunchedEffect(Unit) {
-            window.iconImage = composeLogoAwtImage.await()
-        }
 
         invokeWhenMessageReceived<ApplicationWindowGainedFocus> { event ->
             if (event.windowId == windowId) {
