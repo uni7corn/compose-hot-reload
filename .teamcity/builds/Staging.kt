f8fc332e52117a15853d93b7277991ac09099fb9
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-03-10 20:25:25 +0100

    Use Type.DEPLOYMENT for StagingDeploy

--- .teamcity/builds/Staging.kt
+++ .teamcity/builds/Staging.kt
@@ -1,6 +1,6 @@
 /*
  * Copyright 2024-2025 JetBrains s.r.o. and Compose Hot Reload contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package builds
@@ -14,7 +14,7 @@ import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 object StagingDeploy : BuildType({
     name = "Deploy Staging -> Master"
-    type = Type.COMPOSITE
+    type = Type.DEPLOYMENT
 
 
     vcs {
