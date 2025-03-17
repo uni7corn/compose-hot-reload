ac0fb8bd9cc6a4a67ce438de22dea62bfea9431c
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-03-17 10:58:19 +0100

    Move dev builds to alpha03

--- hot-reload-test/sample/build.gradle.kts
+++ hot-reload-test/sample/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
     jvm()
 
     sourceSets.jvmMain.dependencies {
-        implementation("org.jetbrains.compose.hot-reload:runtime-api:1.0.0-alpha02-74")
+        implementation("org.jetbrains.compose.hot-reload:runtime-api:1.0.0-alpha03-74")
         implementation(compose.runtime)
     }
 
