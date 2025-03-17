f39a26185b1044b0970458ac0b48b6f46b13d467
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-03-17 10:27:49 +0100

    Launch orchestration as first step in agent

--- hot-reload-agent/src/main/kotlin/org/jetbrains/compose/reload/agent/agent.kt
+++ hot-reload-agent/src/main/kotlin/org/jetbrains/compose/reload/agent/agent.kt
@@ -10,8 +10,9 @@ package org.jetbrains.compose.reload.agent
 import java.lang.instrument.Instrumentation
 
 fun premain(@Suppress("unused") args: String?, instrumentation: Instrumentation) {
-    createPidfile()
     launchOrchestration()
+
+    createPidfile()
     launchComposeInstrumentation(instrumentation)
     launchRuntimeTracking(instrumentation)
     launchReloadRequestHandler(instrumentation)
