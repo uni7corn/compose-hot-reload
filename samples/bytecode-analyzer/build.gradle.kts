b5f69b4dfab906d74d28fb86d87874b555b9f082
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-04-02 16:33:58 +0200

    Switch version to 1.0.0-alpha04

--- samples/bytecode-analyzer/build.gradle.kts
+++ samples/bytecode-analyzer/build.gradle.kts
@@ -21,8 +21,8 @@ kotlin {
     }
 
     sourceSets.commonMain.dependencies {
-        implementation("org.jetbrains.compose.hot-reload:core:1.0.0-alpha03-82")
-        implementation("org.jetbrains.compose.hot-reload:analysis:1.0.0-alpha03-82")
+        implementation("org.jetbrains.compose.hot-reload:core:1.0.0-alpha04-82")
+        implementation("org.jetbrains.compose.hot-reload:analysis:1.0.0-alpha04-82")
 
         implementation("io.sellmair:evas:1.2.0")
         implementation("io.sellmair:evas-compose:1.2.0")
--- samples/bytecode-analyzer/settings.gradle.kts
+++ samples/bytecode-analyzer/settings.gradle.kts
@@ -5,7 +5,7 @@
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose.hot-reload") version "1.0.0-alpha03-82"
+        id("org.jetbrains.compose.hot-reload") version "1.0.0-alpha04-82"
     }
 
     repositories {
