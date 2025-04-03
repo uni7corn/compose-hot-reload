3ffdbfe1cecd15df94b583108e694d189cc96be5
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-04-03 09:18:10 +0200

    Switch versioning to 1.0.0-alpha05

--- samples/counter/settings.gradle.kts
+++ samples/counter/settings.gradle.kts
@@ -5,7 +5,7 @@
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose.hot-reload") version "1.0.0-alpha04-83"
+        id("org.jetbrains.compose.hot-reload") version "1.0.0-alpha05-84"
     }
 
     repositories {
