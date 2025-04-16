a7bbab0ed5a25a9ce58abd82dfd4406789055caa
JetBrains Compose Team <compose-team@jetbrains.com>
2025-04-16 09:52:10 +0000

    v1.0.0-alpha06-98

--- hot-reload-test/sample/settings.gradle.kts
+++ hot-reload-test/sample/settings.gradle.kts
@@ -5,8 +5,8 @@
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose.hot-reload") version "1.0.0-alpha06-97"
-        id("org.jetbrains.compose.hot-reload.test") version "1.0.0-alpha06-97"
+        id("org.jetbrains.compose.hot-reload") version "1.0.0-alpha06-98"
+        id("org.jetbrains.compose.hot-reload.test") version "1.0.0-alpha06-98"
     }
 
     repositories {
--- samples/bytecode-analyzer/build.gradle.kts
+++ samples/bytecode-analyzer/build.gradle.kts
@@ -21,8 +21,8 @@ kotlin {
     }
 
     sourceSets.commonMain.dependencies {
-        implementation("org.jetbrains.compose.hot-reload:core:1.0.0-alpha06-97")
-        implementation("org.jetbrains.compose.hot-reload:analysis:1.0.0-alpha06-97")
+        implementation("org.jetbrains.compose.hot-reload:core:1.0.0-alpha06-98")
+        implementation("org.jetbrains.compose.hot-reload:analysis:1.0.0-alpha06-98")
 
         implementation("io.sellmair:evas:1.2.0")
         implementation("io.sellmair:evas-compose:1.2.0")
