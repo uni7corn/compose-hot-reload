fa7171202c0f0c3ba6a4e0757c98f3f9e328c879
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-04-17 14:13:45 +0200

    FunctionalTests: Warmup against Android

--- tests/src/reloadFunctionalTestWarmup/kotlin/org/jetbrains/compose/reload/tests/Warmup.kt
+++ tests/src/reloadFunctionalTestWarmup/kotlin/org/jetbrains/compose/reload/tests/Warmup.kt
@@ -8,6 +8,7 @@ package org.jetbrains.compose.reload.tests
 import org.jetbrains.compose.reload.core.withAsyncTrace
 import org.jetbrains.compose.reload.orchestration.OrchestrationMessage
 import org.jetbrains.compose.reload.orchestration.OrchestrationMessage.UIRendered
+import org.jetbrains.compose.reload.test.gradle.AndroidHotReloadTest
 import org.jetbrains.compose.reload.test.gradle.HotReloadTest
 import org.jetbrains.compose.reload.test.gradle.HotReloadTestFixture
 import org.jetbrains.compose.reload.test.gradle.launchApplication
@@ -22,6 +23,7 @@ class Warmup {
      * Warmup test which shall ensure that 'actual' tests have all dependencies downloaded.
      */
     @HotReloadTest
+    @AndroidHotReloadTest
     @Execution(ExecutionMode.SAME_THREAD)
     fun build(fixture: HotReloadTestFixture) = fixture.runTest(timeout = timeout) {
         fixture.runTransaction {
