8cf9f5d38ba1012c41aab6062b7596bf9d8af2db
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-04-23 07:29:50 +0200

    Update teamcity config to '2025.03'

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -31,6 +31,6 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2024.12"
+version = "2025.03"
 
 project(ComposeHotReloadProject)
