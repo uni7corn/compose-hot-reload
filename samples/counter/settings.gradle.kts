1a3c3c348e7695a3d326a7f963db4ca7eed3aa5c
JetBrains Compose Team <compose-team@jetbrains.com>
2025-05-18 01:05:29 +0000

    v1.0.0-beta01-127

--- samples/counter/settings.gradle.kts
+++ samples/counter/settings.gradle.kts
@@ -5,7 +5,7 @@
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose.hot-reload") version "1.0.0-beta01-126"
+        id("org.jetbrains.compose.hot-reload") version "1.0.0-beta01-127"
     }
 
     repositories {
