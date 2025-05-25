f1622b365f275748231f8d278e62574ddfe8cdd8
JetBrains Compose Team <compose-team@jetbrains.com>
2025-05-25 01:04:49 +0000

    v1.0.0-beta01-129

--- samples/bytecode-analyzer/settings.gradle.kts
+++ samples/bytecode-analyzer/settings.gradle.kts
@@ -5,7 +5,7 @@
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose.hot-reload") version "1.0.0-beta01-128"
+        id("org.jetbrains.compose.hot-reload") version "1.0.0-beta01-129"
     }
 
     repositories {
