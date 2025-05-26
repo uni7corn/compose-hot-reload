4acbd55fc13276a000243bf6ba231604c5f16488
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-05-26 11:36:26 +0200

    Update Kotlin to 2.1.21

--- hot-reload-test/idea-plugin/settings.gradle.kts
+++ hot-reload-test/idea-plugin/settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("jvm") version "2.1.20"
+        kotlin("jvm") version "2.1.21"
     }
 }
 
--- hot-reload-test/sample/settings.gradle.kts
+++ hot-reload-test/sample/settings.gradle.kts
@@ -17,8 +17,8 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("multiplatform") version "2.1.20"
-        kotlin("plugin.compose") version "2.1.20"
+        kotlin("multiplatform") version "2.1.21"
+        kotlin("plugin.compose") version "2.1.21"
         id("org.jetbrains.compose") version "1.7.3"
     }
 }
