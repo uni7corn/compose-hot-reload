a6593538f1f7f69fba149f4335d4028d2d2914e6
JetBrains Compose Team <compose-team@jetbrains.com>
2025-06-11 12:04:16 +0000

    v1.0.0-beta01-139

--- hot-reload-test/sample/build.gradle.kts
+++ hot-reload-test/sample/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
     jvm()
 
     sourceSets.jvmMain.dependencies {
-        implementation("org.jetbrains.compose.hot-reload:runtime-api:1.0.0-beta01-138")
+        implementation("org.jetbrains.compose.hot-reload:runtime-api:1.0.0-beta01-139")
         implementation(compose.runtime)
     }
 
