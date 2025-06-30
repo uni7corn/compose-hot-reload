b4375647c8ccc57be790662831baeef1371a986b
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-06-30 10:47:16 +0200

    Switch versioning schema to .beta.1

--- hot-reload-test/sample/build.gradle.kts
+++ hot-reload-test/sample/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
     jvm()
 
     sourceSets.jvmMain.dependencies {
-        implementation("org.jetbrains.compose.hot-reload:runtime-api:1.0.0-beta01-147")
+        implementation("org.jetbrains.compose.hot-reload:runtime-api:1.0.0-beta.1-147")
         implementation(compose.runtime)
     }
 
