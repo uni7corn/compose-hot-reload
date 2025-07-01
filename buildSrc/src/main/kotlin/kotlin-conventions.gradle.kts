a314079802e4790079fb6550d67a69971cf80e11
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-07-01 10:38:24 +0200

    Build: Use Kotlin Language version 2.1 and stdlib 2.1.21 across the project
    Fixes #258

--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -3,10 +3,14 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
+import gradle.kotlin.dsl.accessors._535c4c276e625605c4ded5a017fa2c29.implementation
 import org.jetbrains.kotlin.gradle.dsl.HasConfigurableKotlinCompilerOptions
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinBasePluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
@@ -21,14 +25,31 @@ plugins.withType<KotlinBasePluginWrapper> {
 
         if (this is HasConfigurableKotlinCompilerOptions<*>) {
             this.compilerOptions {
-                languageVersion = KotlinVersion.KOTLIN_2_2
+                languageVersion = KotlinVersion.KOTLIN_2_1
                 apiVersion = KotlinVersion.KOTLIN_2_1
                 optIn.add("org.jetbrains.compose.reload.InternalHotReloadApi")
                 optIn.add("org.jetbrains.compose.reload.DelicateHotReloadApi")
             }
         }
     }
+
+    val kotlin = kotlinExtension
+    if (kotlin is KotlinMultiplatformExtension) {
+        kotlin.run {
+            kotlin.sourceSets.commonMain.dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.21")
+            }
+        }
+    }
+
+    if (kotlin is KotlinJvmProjectExtension) {
+        dependencies {
+            implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.21")
+        }
+    }
 }
+
+
 
 tasks.withType<KotlinJvmCompile>().configureEach {
     compilerOptions {
