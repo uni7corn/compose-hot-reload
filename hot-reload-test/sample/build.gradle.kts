39dce3f376b13cea2b42a02c0550d01becc0749f
JetBrains Compose Team <compose-team@jetbrains.com>
2025-07-06 01:04:45 +0000

    v1.0.0-beta04+154

--- hot-reload-test/sample/build.gradle.kts
+++ hot-reload-test/sample/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
     jvm()
 
     sourceSets.jvmMain.dependencies {
-        implementation("org.jetbrains.compose.hot-reload:runtime-api:1.0.0-beta04+153")
+        implementation("org.jetbrains.compose.hot-reload:runtime-api:1.0.0-beta04+154")
         implementation(compose.runtime)
     }
 
--- samples/bytecode-analyzer/build.gradle.kts
+++ samples/bytecode-analyzer/build.gradle.kts
@@ -15,8 +15,8 @@ kotlin {
     jvmToolchain(21)
 
     sourceSets.commonMain.dependencies {
-        implementation("org.jetbrains.compose.hot-reload:hot-reload-core:1.0.0-beta04+153")
-        implementation("org.jetbrains.compose.hot-reload:hot-reload-analysis:1.0.0-beta04+153")
+        implementation("org.jetbrains.compose.hot-reload:hot-reload-core:1.0.0-beta04+154")
+        implementation("org.jetbrains.compose.hot-reload:hot-reload-analysis:1.0.0-beta04+154")
 
         implementation("io.sellmair:evas:1.2.0")
         implementation("io.sellmair:evas-compose:1.2.0")
