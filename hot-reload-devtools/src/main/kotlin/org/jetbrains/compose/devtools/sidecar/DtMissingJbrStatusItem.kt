9955d3194cf1b524a9c4beb73fab04e88a5908c9
Azat Abdullin <azat.abdullin@jetbrains.com>
2025-08-13 14:14:29 +0200

    Rework the "Missing JBR" status item: * Use JB icons * Add a dialog window with more info about current JVM

--- hot-reload-devtools/src/main/kotlin/org/jetbrains/compose/devtools/sidecar/DtMissingJbrStatusItem.kt
+++ hot-reload-devtools/src/main/kotlin/org/jetbrains/compose/devtools/sidecar/DtMissingJbrStatusItem.kt
@@ -5,11 +5,22 @@
 
 package org.jetbrains.compose.devtools.sidecar
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
-import org.jetbrains.compose.devtools.theme.DtColors
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import org.jetbrains.compose.devtools.theme.DtImages
+import org.jetbrains.compose.devtools.theme.DtTextStyles
+import org.jetbrains.compose.devtools.widgets.DtErrorDialogWindow
+import org.jetbrains.compose.devtools.widgets.DtImage
 import org.jetbrains.compose.devtools.widgets.DtText
 
 @Composable
@@ -17,8 +28,36 @@ fun DtMissingJbrStatusItem() {
     val vendor = System.getProperty("java.vendor")
     if (vendor.contains("JetBrains", ignoreCase = true)) return
 
+    var isDialogVisible by remember { mutableStateOf(false) }
+
     DtSidecarStatusItem(
-        symbol = { Icon(Icons.Default.Warning, "Missing JBR", tint = DtColors.statusColorWarning) },
-        content = { DtText("Not running on 'JetBrains Runtime' (Current jvm: $vendor)") }
+        symbol = { DtImage(DtImages.Image.WARNING_ICON, "Missing JBR") },
+        content = {
+            DtText(
+                "Not running on 'JetBrains Runtime' (Current jvm: $vendor)",
+                modifier = Modifier
+                    .pointerHoverIcon(PointerIcon.Hand)
+                    .clickable(role = Role.Button) { isDialogVisible = !isDialogVisible },
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = DtTextStyles.code.copy(
+                    textDecoration = TextDecoration.Underline,
+                )
+            )
+        }
     )
+
+    if (isDialogVisible) {
+        DtErrorDialogWindow(
+            title = "Not running on 'JetBrains Runtime'",
+            message = "Current JVM info",
+            logs = buildList {
+                add("JVM name: ${System.getProperty("java.vm.name") ?: "N/A"}")
+                add("JVM vendor: ${System.getProperty("java.vm.vendor") ?: "N/A"}")
+                add("JVM version: ${System.getProperty("java.vm.version") ?: "N/A"}")
+                add("JVM home: ${System.getProperty("java.home") ?: "N/A"}")
+            },
+            onCloseRequest = { isDialogVisible = false }
+        )
+    }
 }
