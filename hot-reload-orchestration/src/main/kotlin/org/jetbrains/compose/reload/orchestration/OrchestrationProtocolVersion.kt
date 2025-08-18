bab966f3a67fea958911860882f38ee23791815d
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-08-18 09:48:11 +0200

    Deprecate 'OrchestrationProtocolVersion' in favor of 'OrchestrationVersion'
    The enum implementation of the 'OrchestrationProtocolVersion' makes it hard to work with
    'unknown' versions of the protocol (e.g. an older Server working with a client with a not familiar
    higher version)

--- hot-reload-orchestration/src/main/kotlin/org/jetbrains/compose/reload/orchestration/OrchestrationProtocolVersion.kt
+++ hot-reload-orchestration/src/main/kotlin/org/jetbrains/compose/reload/orchestration/OrchestrationProtocolVersion.kt
@@ -9,13 +9,15 @@ import org.jetbrains.compose.reload.core.Try
 import org.jetbrains.compose.reload.core.toLeft
 import org.jetbrains.compose.reload.core.toRight
 
+@Deprecated("Use 'OrchestrationVersion' instead.", ReplaceWith("OrchestrationVersion"))
 public enum class OrchestrationProtocolVersion(public val intValue: Int) {
-    V1(1);
+    V1(1),
+    V1_1(2);
 
     public companion object {
         internal const val serialVersionUID: Long = 0L
 
-        public val current: OrchestrationProtocolVersion get() = V1
+        public val current: OrchestrationProtocolVersion get() = V1_1
 
         public fun from(intValue: Int): Try<OrchestrationProtocolVersion> {
             entries.firstOrNull { it.intValue == intValue }?.let { return it.toLeft() }
@@ -23,3 +25,26 @@ public enum class OrchestrationProtocolVersion(public val intValue: Int) {
         }
     }
 }
+
+public data class OrchestrationVersion(public val intValue: Int) : Comparable<OrchestrationVersion> {
+    public companion object {
+        public val v1: OrchestrationVersion = OrchestrationVersion(1)
+        public val v1_1: OrchestrationVersion = OrchestrationVersion(2)
+        public val current: OrchestrationVersion = v1_1
+    }
+
+    override fun compareTo(other: OrchestrationVersion): Int {
+        return this.intValue.compareTo(other.intValue)
+    }
+
+    override fun toString(): String {
+        return when (this) {
+            v1 -> "v1($intValue)"
+            v1_1 -> "v1.1($intValue)"
+            else -> "N/A($intValue)"
+        }
+    }
+}
+
+internal val OrchestrationVersion.supportsStates: Boolean
+    get() = this >= OrchestrationVersion.v1_1
