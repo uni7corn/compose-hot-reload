3ac66ff06ee85b036ca53d8e779f9af06c3b3032
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-10-22 10:05:26 +0200

    OrchestrationListenerIntegrationTest: Use 'OrchestrationConnectionState' instead of 'ClientConnected'
    This should help mitigating a race condition between the deferred connection
    and the application to connect.

--- tests/src/reloadFunctionalTest/kotlin/org/jetbrains/compose/reload/tests/OrchestrationListenerIntegrationTest.kt
+++ tests/src/reloadFunctionalTest/kotlin/org/jetbrains/compose/reload/tests/OrchestrationListenerIntegrationTest.kt
@@ -6,9 +6,6 @@
 package org.jetbrains.compose.reload.tests
 
 import kotlinx.coroutines.currentCoroutineContext
-import kotlinx.coroutines.flow.consumeAsFlow
-import kotlinx.coroutines.flow.filterIsInstance
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.job
@@ -19,8 +16,6 @@ import org.jetbrains.compose.reload.orchestration.OrchestrationClientRole
 import org.jetbrains.compose.reload.orchestration.OrchestrationClientRole.Application
 import org.jetbrains.compose.reload.orchestration.OrchestrationConnectionsState
 import org.jetbrains.compose.reload.orchestration.OrchestrationMessage
-import org.jetbrains.compose.reload.orchestration.OrchestrationMessage.ClientConnected
-import org.jetbrains.compose.reload.orchestration.asChannel
 import org.jetbrains.compose.reload.orchestration.sendAsync
 import org.jetbrains.compose.reload.orchestration.startOrchestrationListener
 import org.jetbrains.compose.reload.orchestration.toJvmArg
@@ -68,8 +63,6 @@ class OrchestrationListenerIntegrationTest {
             client.close()
         }
 
-        val messages = client.asChannel()
-
         withAsyncTrace("Await deferred client connection") {
             client.states.get(OrchestrationConnectionsState).await { state ->
                 client.clientId in state.connections.map { it.clientId }
@@ -77,9 +70,9 @@ class OrchestrationListenerIntegrationTest {
         }
 
         val application = withAsyncTrace("Await application to connect") {
-            messages.consumeAsFlow().filterIsInstance<ClientConnected>().first {
-                it.clientRole == Application
-            }
+            client.states.get(OrchestrationConnectionsState).await { state ->
+                Application in state.connections.map { it.clientRole }
+            }.connections.first { it.clientRole == Application }
         }
 
         val applicationProcessHandle = ProcessHandle.of(application.clientPid!!).get()
