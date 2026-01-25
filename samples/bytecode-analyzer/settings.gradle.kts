2fdbf5eb6201a4802b7b8f0f6fbc1b5769e7e861
JetBrains Compose Team <compose-team@jetbrains.com>
2026-01-25 02:06:50 +0000

    v1.1.0-alpha+188

--- samples/bytecode-analyzer/settings.gradle.kts
+++ samples/bytecode-analyzer/settings.gradle.kts
@@ -5,7 +5,7 @@
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose.hot-reload") version "1.1.0-alpha+187"
+        id("org.jetbrains.compose.hot-reload") version "1.1.0-alpha+188"
     }
 
     repositories {
@@ -23,7 +23,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.compose.hot-reload.test.jbr-resolver-convention") version "1.1.0-alpha+187"
+    id("org.jetbrains.compose.hot-reload.test.jbr-resolver-convention") version "1.1.0-alpha+188"
 }
 
 dependencyResolutionManagement {
