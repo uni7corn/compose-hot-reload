d8bde8466f9781e289336e0718a506002f70000b
JetBrains Compose Team <compose-team@jetbrains.com>
2026-03-22 02:04:44 +0000

    v1.1.0-beta+195

--- hot-reload-test/sample/settings.gradle.kts
+++ hot-reload-test/sample/settings.gradle.kts
@@ -5,8 +5,8 @@
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose.hot-reload") version "1.1.0-beta+194"
-        id("org.jetbrains.compose.hot-reload.test") version "1.1.0-beta+194"
+        id("org.jetbrains.compose.hot-reload") version "1.1.0-beta+195"
+        id("org.jetbrains.compose.hot-reload.test") version "1.1.0-beta+195"
     }
 
     repositories {
--- samples/bytecode-analyzer/settings.gradle.kts
+++ samples/bytecode-analyzer/settings.gradle.kts
@@ -5,7 +5,7 @@
 
 pluginManagement {
     plugins {
-        id("org.jetbrains.compose.hot-reload") version "1.1.0-beta+194"
+        id("org.jetbrains.compose.hot-reload") version "1.1.0-beta+195"
     }
 
     repositories {
@@ -23,7 +23,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.compose.hot-reload.test.jbr-resolver-convention") version "1.1.0-beta+194"
+    id("org.jetbrains.compose.hot-reload.test.jbr-resolver-convention") version "1.1.0-beta+195"
 }
 
 dependencyResolutionManagement {
