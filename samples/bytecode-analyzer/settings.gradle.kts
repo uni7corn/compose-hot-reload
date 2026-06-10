2a4ae94d112252b0efd77cb7f958ec479784d40e
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2026-06-10 17:36:31 +0300

    [samples] Update Compose version to 1.10.0

--- samples/bytecode-analyzer/settings.gradle.kts
+++ samples/bytecode-analyzer/settings.gradle.kts
@@ -20,7 +20,7 @@ pluginManagement {
     plugins {
         kotlin("multiplatform") version "2.3.21"
         kotlin("plugin.compose") version "2.3.21"
-        id("org.jetbrains.compose") version "1.8.2"
+        id("org.jetbrains.compose") version "1.10.0"
     }
 }
 
